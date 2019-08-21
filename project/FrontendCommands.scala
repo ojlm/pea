c2991386a5389a28ef2f8f11ae02912b4c514d82
zhengshaodong <ngxcorpio@gmail.com>
2019-08-21 10:20:05 +0800

    Disable frontend test during ci

--- project/FrontendCommands.scala
+++ project/FrontendCommands.scala
@@ -1,7 +1,7 @@
 object FrontendCommands {
 
   val dependencyInstall: String = "yarn install"
-  val test: String = "yarn run test:ci"
+  val test: String = "yarn run test:none"
   val serve: String = "yarn run start"
   val build: String = "yarn run build:prod"
 }
