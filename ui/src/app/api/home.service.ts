af0da3519a2364590fe45501de7ecb2d8718ce2e
zhengshaodong <ngxcorpio@gmail.com>
2019-08-25 22:13:49 +0800

    Add declarations of models and api

--- ui/src/app/api/home.service.ts
+++ ui/src/app/api/home.service.ts
@@ -2,7 +2,14 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
 import { ApiRes } from '../model/api.model'
-import { PeaMember } from '../model/pea.model'
+import {
+  PeaMember,
+  ReporterJobStatus,
+  RunSimulationJob,
+  Simulations,
+  SingleHttpScenarioJob,
+  WorkersAvailable,
+} from '../model/pea.model'
 import { BaseService } from './base.service'
 
 @Injectable({
@@ -16,11 +23,27 @@ export class HomeService extends BaseService {
     return this.http.get<ApiRes<string[]>>(`${this.API_BASE}/jobs`)
   }
 
+  getJobDetails(runId: string) {
+    return this.http.get<ApiRes<ReporterJobStatus>>(`${this.API_BASE}/job/${runId}`)
+  }
+
   getLocalReports() {
     return this.http.get<ApiRes<string[]>>(`${this.API_BASE}/reports`)
   }
 
   getWorkers() {
     return this.http.get<ApiRes<PeaMember[]>>(`${this.API_BASE}/workers`)
+  }
+
+  getSimulations() {
+    return this.http.get<ApiRes<Simulations>>(`${this.API_BASE}/simulations`)
+  }
+
+  runSingleHttpScenarioJob(load: SingleHttpScenarioJob) {
+    return this.http.post<ApiRes<WorkersAvailable>>(`${this.API_BASE}/single`, load)
+  }
+
+  runSimulationJob(load: RunSimulationJob) {
+    return this.http.post<ApiRes<WorkersAvailable>>(`${this.API_BASE}/simulation`, load)
   }
 }
--- ui/src/app/model/pea.model.ts
+++ ui/src/app/model/pea.model.ts
@@ -33,3 +33,113 @@ export interface RequestCounters {
   successfulCount?: number
   failedCount?: number
 }
+
+export interface JobWorkerStatus {
+  status?: string
+  errMsg?: string
+}
+
+export interface ReporterJobStatus {
+  status?: string
+  runId?: string
+  start?: number
+  end?: number
+  workers?: { [k: string]: JobWorkerStatus }
+}
+
+export interface SimulationModel {
+  name?: string
+  description?: string
+}
+
+export interface Simulations {
+  last?: number
+  simulations?: SimulationModel[]
+}
+
+export interface SingleRequest {
+  name?: string
+  url?: string
+  method?: string
+  headers?: Object
+  body?: string
+}
+
+export interface During {
+  value?: number
+  unit?: string
+}
+
+export interface Injection {
+  type?: string
+  users?: number
+  to?: number
+  during?: During
+}
+
+export interface SingleHttpScenarioMessage {
+  name?: string
+  request?: SingleRequest
+  injections?: Injection[]
+  report?: boolean
+  simulationId?: string
+  start?: number
+}
+
+export interface RunSimulationMessage {
+  simulation?: string
+  report?: boolean
+  simulationId?: string
+  start?: number
+}
+
+export interface SingleHttpScenarioJob {
+  workers?: PeaMember[]
+  request?: SingleHttpScenarioMessage
+}
+
+export interface RunSimulationJob {
+  workers?: PeaMember[]
+  request?: RunSimulationMessage
+}
+
+export interface WorkersAvailable {
+  available?: boolean
+  errors?: { [k: string]: string }
+  runId?: string
+}
+
+export const TimeUnit = {
+  TIME_UNIT_MILLI: 'milli',
+  TIME_UNIT_SECOND: 'second',
+  TIME_UNIT_MINUTE: 'minute',
+  TIME_UNIT_HOUR: 'hour',
+}
+
+export const InjectionType = {
+  TYPE_RAMP_USERS: 'rampUsers',
+  TYPE_HEAVISIDE_USERS: 'heavisideUsers',
+  TYPE_AT_ONCE_USERS: 'atOnceUsers',
+  TYPE_CONSTANT_USERS_PER_SEC: 'constantUsersPerSec',
+  TYPE_RAMP_USERS_PER_SEC: 'rampUsersPerSec',
+}
+
+export const HttpMethods = {
+  OPTIONS: 'OPTIONS',
+  GET: 'GET',
+  HEAD: 'HEAD',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+  TRACE: 'TRACE',
+}
+
+export const MemberStatus = {
+  IDLE: 'idle',
+  RUNNING: 'running',
+  IIL: 'ill',
+  GATHERING: 'gathering',
+  REPORTING: 'reporting',
+  FINISHED: 'finished',
+}
